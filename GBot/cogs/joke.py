5bb1e7ebc4c4205b25bf4076f9977992d0b81f31
BuildTools <unconfigured@null.spigotmc.org>
2021-11-01 23:11:02 +0900

    fix: joke.pyのスラッシュコマンドを削除

--- GBot/cogs/joke.py
+++ GBot/cogs/joke.py
@@ -6,14 +6,6 @@ class Joke(commands.Cog):
     def __init__(self, bot: GBot):
         self.bot = bot
 
-    @GBot.slash_command()
-    async def _gold(self, ctx, Quantity):
-        await ctx.send(f"金塊を{Quantity}個生成しました。")
-
-    @GBot.slash_command()
-    async def _JS(self, ctx):
-        await ctx.send(f"{ctx.author.mention}JS最強！")
-
 
 def setup(bot):
     return bot.add_cog(Joke(bot))
