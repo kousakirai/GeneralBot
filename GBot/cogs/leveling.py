90865acca3c28a8fbd85ee72d2401b9eb9f87889
BuildTools <unconfigured@null.spigotmc.org>
2021-11-02 18:26:52 +0900

    create: レベル機能のキューシステムの作りかけ

--- GBot/cogs/leveling.py
+++ GBot/cogs/leveling.py
@@ -1,10 +1,28 @@
-from discord.ext import commands
+from discord.ext import commands, tasks
 from GBot.core.bot import GBot
+from GBot.models.auth import Level
+import random
 
 
 class leveling(commands.Cog):
     def __init__(self, bot: GBot):
         self.bot = bot
+
+    async def create_level(self, user_id):
+        num = random.randint(0, 5)
+        level = Level.create(user_id=user_id)
+        level = level.get()
+        level.set(exp=num)
+
+    @tasks.loop(seconds=60)
+    async def update_level(self, user_id):
+        num = random.randint(0, 5)
+        level = Level(user_id)
+
+    @commands.Cog.listener()
+    async def on_message(self, message):
+        level = Level(message.author.id).get()
+        if level < 0:
 
 
 def setup(bot):
---
+++ GBot/models/level.py
@@ -0,0 +1,26 @@
+from GBot.models import model
+
+modelname = model.Level
+
+
+class Level:
+    def __init__(self, user_id):
+        self.user_id = user_id
+
+    def get(self):
+        rest = modelname.objects.filter(id=self.user_id).first()
+        return rest
+
+    def set(self, **kwargs):
+        rest = modelname.objects.filter(id=self.user_id).update_one(**kwargs)
+        return rest
+
+    def delete(self):
+        rest = modelname.objects.filter(id=self.user_id).first().delete()
+        return rest
+
+    @classmethod
+    def create(cls, *, user_id):
+        rest = modelname(id=user_id)
+        rest.save()
+        return rest
