67343341912e3e4375d96c84dd1491008679a151
minato37103710 <81461409+minato37103710@users.noreply.github.com>
2021-11-09 17:06:54 +0900

    typo?

--- GBot/cogs/afk.py
+++ GBot/cogs/afk.py
@@ -6,9 +6,9 @@ class afk(commands.Cog):
     def __init__(self, bot: GBot):
         self.bot = bot
 
-@commands.command(name='afk')
-async def afk(self,ctx,reason):
-    await ctx.send(reason)
+    @commands.command(name='afk')
+    async def afk(self,ctx,reason):
+        await ctx.send(reason)
 
 def setup(bot):
     return bot.add_cog(
